57c727ba915450c05e454a621b15491a5a4a6366
kanemaki <kanemaki@gmail.com>
2016-06-26 21:41:37 -0300

    Desenvolvimento

--- app/src/main/java/br/eti/kanemaki/testejson/MainActivity.java
+++ app/src/main/java/br/eti/kanemaki/testejson/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageButton;
 
@@ -18,18 +20,22 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.MyAdapterRecycler;
 import model.OkHttpHandler;
+import model.WebHead;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     // revistas JSONArray
     JSONArray banca = null;
-    private static final String URL = "http://i.annihil.us/u/prod/marvel/i/mg/4/b0/52ded58839768/portrait_uncanny.jpg";
-    private static final String URL1 = "http://i.annihil.us/u/prod/marvel/i/mg/c/40/57584aabce22f/portrait_uncanny.jpg";
-    private static final String URL2 = "http://i.annihil.us/u/prod/marvel/i/mg/2/10/52cee2f4eea1d/portrait_uncanny.jpg";
-    private static final String URL3 = "http://i.annihil.us/u/prod/marvel/i/mg/6/80/575847a14cc4c/portrait_uncanny.jpg";
+    //private static final String URL = "http://i.annihil.us/u/prod/marvel/i/mg/4/b0/52ded58839768/portrait_uncanny.jpg";
+    //private static final String URL1 = "http://i.annihil.us/u/prod/marvel/i/mg/c/40/57584aabce22f/portrait_uncanny.jpg";
+    //private static final String URL2 = "http://i.annihil.us/u/prod/marvel/i/mg/2/10/52cee2f4eea1d/portrait_uncanny.jpg";
+    //private static final String URL3 = "http://i.annihil.us/u/prod/marvel/i/mg/6/80/575847a14cc4c/portrait_uncanny.jpg";
 
 
     private static final String TAG_REVISTA = "data";
@@ -48,16 +54,87 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG_CAPAS = "thumbnail";
     private static final String TAG_CAPAS_PATH = "path";
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        downloadMarvel();
-        dowloadImagem(URL);
-        dowloadImagem1(URL1);
-        dowloadImagem2(URL2);
-        dowloadImagem3(URL3);
+        mRecyclerView = (RecyclerView) findViewById(R.id.lv_default);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        List<WebHead> listaSP = new ArrayList<>();
+
+        OkHttpHandler handler = new OkHttpHandler();
+
+        String marvelStringJson = "";
+        String marvelString = "";
+        try {
+            marvelStringJson = handler.execute().get();
+
+            Log.d("Response: ", "> " + marvelStringJson);
+
+            if (marvelStringJson != null){
+                try {
+                    JSONObject jsonObj = new JSONObject(marvelStringJson);
+
+                    // Getting JSON Array node
+                    JSONObject hq = jsonObj.getJSONObject(TAG_REVISTA);
+                    banca = hq.getJSONArray(TAG_RESULTS);
+
+                    // looping through All HQS
+                    for (int i = 0; i < banca.length(); i++) {
+                        JSONObject revista = banca.getJSONObject(i);
+
+                        String id = revista.getString(TAG_ID).toString();
+                        String numero = revista.getString(TAG_NUM_REVISTA).toString();
+                        String titulo = revista.getString(TAG_TITULO).toString();
+                        String desc = revista.getString(TAG_DESCRI).toString();
+                        String paginas = revista.getString(TAG_PAGINAS).toString();
+
+                        JSONObject img = revista.getJSONObject(TAG_CAPAS);
+                        String image =  img.getString(TAG_CAPAS_PATH).toString();
+
+                        JSONArray data = revista.getJSONArray(TAG_DATAS);
+                        JSONObject dt = data.getJSONObject(0);
+                        String hq_Data =  dt.getString(TAG_DATA).toString();
+
+                        JSONArray preco = revista.getJSONArray(TAG_PRECOS);
+                        JSONObject prc = preco.getJSONObject(0);
+                        String hq_Prc =  prc.getString(TAG_PRECO).toString();
+
+                        Log.d("Response: ", " <------------------------------> " + i);
+                        Log.d("Response: ", " id > " + id);
+                        Log.d("Response: ", " numero > " + numero);
+                        Log.d("Response: ", " titulo > " + titulo);
+                        Log.d("Response: ", " descrição > " + desc);
+                        Log.d("Response: ", " paginas > " + paginas);
+                        Log.d("Response: ", " imagens > " + image);
+                        Log.d("Response: ", " data > " + hq_Data);
+                        Log.d("Response: ", " preço > " + hq_Prc);
+                        Log.d("Response: ", " <------------------------------> ");
+
+                        listaSP.add(new WebHead("#" + numero, image + "/portrait_fantastic.jpg"));
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+
+        }
+
+
+        MyAdapterRecycler adpter = new MyAdapterRecycler(listaSP, this);
+
+        mRecyclerView.setAdapter(adpter);
 
     }
 
@@ -123,117 +200,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void dowloadImagem (final String urlImg){
-        new Thread(){
-            @Override
-            public void run(){
-                try{
-                    URL url = new URL(urlImg);
-                    InputStream in = url.openStream();
-                    final Bitmap imagem = BitmapFactory.decodeStream(in);
-                    in.close();
-                    atualizaImagem(imagem);
-                }catch(IOException e){
 
-                }
-            }
-        }.start();
-
-    }
-
-    private void atualizaImagem(final Bitmap imagem){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ImageButton imgView = (ImageButton) findViewById(R.id.imageButton);
-                imgView.setImageBitmap(imagem);
-            }
-        });
-    }
-
-    private void dowloadImagem1 (final String urlImg){
-        new Thread(){
-            @Override
-            public void run(){
-                try{
-                    URL url = new URL(urlImg);
-                    InputStream in = url.openStream();
-                    final Bitmap imagem = BitmapFactory.decodeStream(in);
-                    in.close();
-                    atualizaImagem1(imagem);
-                }catch(IOException e){
-
-                }
-            }
-        }.start();
-
-    }
-
-    private void atualizaImagem1(final Bitmap imagem){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ImageButton imgView = (ImageButton) findViewById(R.id.imageButton1);
-                imgView.setImageBitmap(imagem);
-            }
-        });
-    }
-
-    private void dowloadImagem2 (final String urlImg){
-        new Thread(){
-            @Override
-            public void run(){
-                try{
-                    URL url = new URL(urlImg);
-                    InputStream in = url.openStream();
-                    final Bitmap imagem = BitmapFactory.decodeStream(in);
-                    in.close();
-                    atualizaImagem2(imagem);
-                }catch(IOException e){
-
-                }
-            }
-        }.start();
-
-    }
-
-    private void atualizaImagem2(final Bitmap imagem){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ImageButton imgView = (ImageButton) findViewById(R.id.imageButton2);
-                imgView.setImageBitmap(imagem);
-            }
-        });
-    }
-
-    private void dowloadImagem3 (final String urlImg){
-        new Thread(){
-            @Override
-            public void run(){
-                try{
-                    URL url = new URL(urlImg);
-                    InputStream in = url.openStream();
-                    final Bitmap imagem = BitmapFactory.decodeStream(in);
-                    in.close();
-                    atualizaImagem3(imagem);
-                }catch(IOException e){
-
-                }
-            }
-        }.start();
-
-    }
-
-    private void atualizaImagem3(final Bitmap imagem){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ImageButton imgView = (ImageButton) findViewById(R.id.imageButton3);
-                imgView.setImageBitmap(imagem);
-            }
-        });
-    }
 }
 
 //  Deve conter:  ○ Miniatura da capa da revista (thumbnail); 
